db54eee1b55010ebf531e480f6c428aaf76f02d3
Galvez <javier.galvez@assaabloy.com>
2023-11-16 18:45:02 -0300

    act-16/11

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask, jsonify, request, make_response
 from flask_cors import CORS
+import os, uuid
 from flask_bcrypt import Bcrypt
+from werkzeug.utils import secure_filename
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
-from models import db, User
+from models import db, Users, Profiles, Users_profiles, Type, Model, Brand, Pc_users, State, Pc, State_pc
 
 app = Flask(__name__)
 
 # Configuración base de datos
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/test_login_base'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/afn'
 
+#folder pdf
+app.config['UPLOAD_FOLDER'] = './pdf_afn'
 
 # Configuración de JWT
 app.config['JWT_SECRET_KEY'] = '9123'
@@ -25,11 +29,11 @@ def login():
         username = request.form['username']
         password = request.form['password']
 
-        user = User.query.filter_by(username=username).first()
+        user = Users.query.filter_by(nick_name=username).first()
 
-        if user and bcrypt.check_password_hash(user.password, password):
+        if user and bcrypt.check_password_hash(user.u_password, password):
             # Crear un token de acceso
-            access_token = create_access_token(identity=user.id)
+            access_token = create_access_token(identity=user.cod_users)
             response = make_response(jsonify(access_token=access_token), 200)
             response.set_cookie('access_token_cookie', value=access_token, httponly=True, samesite='None',secure=True)
 
@@ -49,12 +53,12 @@ def register():
         username = request.form['username']
         password = request.form['password']
         
-        existe_user = User.query.filter_by(username=username).first()
+        existe_user = Users.query.filter_by(nick_name=username).first()
         if existe_user:
             return jsonify({'message': 'El nombre de usuario ya está en uso. Elige otro.'}), 400
 
         hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
-        new_user = User(username=username, password=hashed_password)
+        new_user = Users(nick_name=username, u_password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
 
@@ -66,15 +70,168 @@ def register():
 @jwt_required()
 def check_auth():
     current_user_id = get_jwt_identity()
-    current_user = User.query.get(current_user_id)
-    return jsonify({'authenticated': True, 'user_id': current_user.id, 'username': current_user.username})
+    current_user = Users.query.get(current_user_id)
+    return jsonify({'authenticated': True, 'cod_users': current_user.cod_users, 'nick_name': current_user.nick_name})
 
-@app.route('/listado')
+#carga pdf ¿donde?
+@app.route('/upload_pdf', methods=['POST'])
+def uploader():
+    if request.method == 'POST':
+        pdf = request.files['archivo']
+        if pdf:
+            filename = secure_filename(pdf.filename)
+            unique_filename = str(uuid.uuid4()) +'_'+ filename
+            pdf.save(os.path.join(app.config['UPLOAD_FOLDER'], f'_id_{unique_filename}'))
+            return jsonify({'message': 'Archivo guardado exitosamente'})
+        else:
+            return jsonify({'error': 'No se ha enviado un archivo válido'}), 400
+    else:
+        return jsonify({'error': 'No se encontró el archivo en la solicitud'}), 400
+
+
+@app.route('/add_user_pc', methods= ['POST'])
+@jwt_required()
+def add_user_pc():
+    if request.method == 'POST':
+        lastname_user = request.form['lastname_user']
+        create_date = request.form['create_date']
+
+        new_user_pc = Pc_users(lastname_user=lastname_user,create_date=create_date)
+        db.session.add(new_user_pc)
+        db.session.commit()
+        return jsonify({"estado":"usuario creado"})
+    return jsonify({"estado":"Error al agregar"})
+
+@app.route('/add_pc', methods = ['POST'])
+@jwt_required()
+def create():
+    if request.method == 'POST':
+        name_computer = request.form['namecomputer']
+        serial_name = request.form['nameserial']
+        date_received = request.form['datereceived']
+        cod_model_id = request.form['namemodel']
+        cod_brand_id = request.form['namebrand']
+        cod_type_id = request.form['nametype']
+        #usuario login
+        current_user_id = get_jwt_identity()
+        current_user = Users.query.get(current_user_id)
+
+        existe_user = Pc.query.filter_by(serial_number=serial_name).first()
+        if existe_user:
+            return jsonify({'message': 'El nombre de computador ya está en uso. Elige otro.'}), 400
+
+        new_pc = Pc(name_computer=name_computer,serial_number=serial_name,date_received=date_received,cod_model_id=cod_model_id,cod_brand_id=cod_brand_id,cod_type_id=cod_type_id,cod_users_id=current_user.cod_users)
+        db.session.add(new_pc)
+
+        estado = State.query.filter_by(name_state='no asignado').first()
+        new_statu_pc = State_pc(cod_pc_id=new_pc.cod_pc,cod_state_id=estado.cod_state)
+        db.session.add(new_statu_pc)
+        db.session.commit()
+
+        return jsonify({'pc':'Creado'})
+        
+    return jsonify({'pc':'Error ingreso'})
+
+@app.route('/datos', methods = ['GET'])
+def datos():
+
+    models = Model.query.all()
+    types = Type.query.all()
+    brands = Brand.query.all()
+    
+    models_json = [model.obtener() for model in models]
+    types_json = [tipo.obtener() for tipo in types]
+    brand_json = [brand.obtener() for brand in brands]
+    datos = {
+        'model':models_json,
+        'type':types_json,
+        'brand': brand_json
+    }
+    return jsonify(datos)    
+
+
+@app.route('/get_pc', methods = ['GET','POST'])
+def get_pc():
+
+    resultado = db.session.query(Pc,Model,Brand,Type,State_pc,State)\
+                .join(Model, Pc.cod_model_id==Model.cod_model)\
+                .join(Brand, Pc.cod_brand_id==Brand.cod_brand)\
+                .join(Type, Pc.cod_type_id==Type.cod_type)\
+                .join(State_pc, Pc.cod_pc==State_pc.cod_pc_id)\
+                .join(State, State.cod_state == State_pc.cod_state_id )\
+                .add_columns(Pc.cod_pc,Pc.name_computer,Pc.serial_number,Pc.date_received,Model.name,Brand.name,Type.name,State.name_state)\
+                .order_by(Pc.date_received.desc()).all()
+    db.session.commit()
+
+    data = []
+
+    for computer in resultado:
+        data.append({
+            'cod_pc': computer.Pc.cod_pc,
+            'name_computer': computer.Pc.name_computer,
+            'serial_number': computer.Pc.serial_number,
+            'date_received': computer.Pc.date_received,
+            'name_model': computer.Model.name,
+            'name_brand': computer.Brand.name,
+            'name_type': computer.Type.name,
+            'name_state': computer.State.name_state
+        })       
+
+    return jsonify(data)
+
+@app.route('/listado', methods = ['GET','POST'])
 @jwt_required()
 def listado():
     current_user_id = get_jwt_identity()
-    current_user = User.query.get(current_user_id)
-    return f'Hello, {current_user.username}!'
+    current_user = Users.query.get(current_user_id)
+    return f'Hello, {current_user.nick_name}!'
+
+
+
+
+@app.route('/datos_prueba')
+def addu():
+
+    new_type = Type(name='Notebook')
+    new_type2 = Type(name='Netbook')
+    new_profile = Profiles(name='Administrador')
+    new_profile2 = Profiles(name='Usuario')
+    new_brand = Brand(name='Dell')
+    new_brand2 = Brand(name='Lenovo')
+    new_brand3 = Brand(name='HP')
+    new_model = Model(name='modelo1')
+    new_model2 = Model(name='modelo2')
+    new_model3 = Model(name='modelo3')
+    new_state = State(name_state='Nuevo')
+    new_state2 = State(name_state='Asignado')
+    new_state3 = State(name_state='No asignado')
+    new_state4 = State(name_state='Mal estado')
+    new_state5 = State(name_state='Bueno')
+    new_user = Users(nick_name='javier', u_password='$2b$12$9q6oTe2dQoV/YdPIfrVAZeFA4P3fZYdHfQzHmZkeatX2dJto/eiRW')
+    db.session.add(new_user)
+    db.session.add(new_type)
+    db.session.add(new_type2)
+    db.session.add(new_profile)
+    db.session.add(new_profile2)
+    db.session.add(new_brand)
+    db.session.add(new_brand2)
+    db.session.add(new_brand3)
+    db.session.add(new_model)
+    db.session.add(new_model2)
+    db.session.add(new_model3)
+    db.session.add(new_state)
+    db.session.add(new_state2)
+    db.session.add(new_state3)
+    db.session.add(new_state4)
+    db.session.add(new_state5)
+    db.session.commit()
+
+    return jsonify({'datos':'Cargados'})
+
+with app.app_context():
+    db.create_all()
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,12 +1,91 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+#from flask_login import UserMixin
 
 db = SQLAlchemy()
+#UserMixin
+class Users(db.Model):
+    __tabalename__ = 'users'
+    cod_users = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nick_name = db.Column(db.String(20), nullable=False, unique=True)
+    u_password = db.Column(db.String(80), nullable=False)
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False, unique=True)
-    password = db.Column(db.String(80), nullable=False)
+class Profiles(db.Model):
+    __tabalename__ = 'profiles'
+    cod_profile = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(30))
+
+class Users_profiles(db.Model):
+    __tabalename__ = 'users_profiles'
+    cod_profile = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    cod_users_id = db.Column(db.Integer, db.ForeignKey('users.cod_users'))
+    cod_profile_id = db.Column(db.Integer, db.ForeignKey('profiles.cod_profile'))
+
+class Type(db.Model):
+    __tabalename__ = 'type'
+    cod_type = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(20))
+
+    def obtener(self):
+        return{
+            'cod_type':self.cod_type,
+            'name':self.name
+        }
+
+class Model(db.Model):
+    __tabalename__ = 'model'
+    cod_model = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(20))
+
+    def obtener(self):
+        return{
+            'cod_model':self.cod_model,
+            'name':self.name
+        }
+
+class Brand(db.Model):
+    __tabalename__ = 'brand'
+    cod_brand = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(20))
+
+    def obtener(self):
+        return{
+            'cod_brand':self.cod_brand,
+            'name':self.name
+        }
+
+class Pc(db.Model):
+    __tablename__= 'pc'
+    cod_pc = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name_computer = db.Column(db.String(30))
+    serial_number = db.Column(db.String(50), nullable=False, unique=True)
+    date_received = db.Column(db.DateTime)
+    cod_model_id = db.Column(db.Integer, db.ForeignKey('model.cod_model'))
+    cod_brand_id = db.Column(db.Integer, db.ForeignKey('brand.cod_brand'))
+    cod_type_id = db.Column(db.Integer, db.ForeignKey('type.cod_type'))
+    cod_users_id = db.Column(db.Integer, db.ForeignKey('users.cod_users'))
+
+
+class State(db.Model):
+    __tablename__= 'state'
+    cod_state = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name_state = db.Column(db.String(20))
+
+
+class State_pc(db.Model):
+    __tabalename__ = 'state_pc'
+    cod_state_pc = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    cod_pc_id = db.Column(db.Integer, db.ForeignKey('pc.cod_pc'))
+    cod_state_id = db.Column(db.Integer, db.ForeignKey('state.cod_state'))
+    cod_pusers_id = db.Column(db.Integer, db.ForeignKey('pc_users.cod_pusers'))
+    deliver_date = db.Column(db.DateTime)
+    commnet = db.Column(db.String(100))
+
+class Pc_users(db.Model):
+    __tabalename__ = 'pc_users'
+    cod_pusers = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    lastname_user = db.Column(db.String(100))
+    create_date = db.Column(db.DateTime)
+    
 
 
 
@@ -14,3 +93,7 @@ class User(db.Model, UserMixin):
 #crear tablas en db
 #with app.app_context():
 #    db.create_all()
+
+
+
+
